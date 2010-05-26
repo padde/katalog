d3087178f057cdc7cc3ea51ff40149f96f00c19f
Patrick Oscity <patrick.oscity@gmail.com>
2010-05-26 04:11:03 +0200

    group scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100526020602) do
+ActiveRecord::Schema.define(:version => 20100526021029) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "items", :force => true do |t|
     t.string   "title"
