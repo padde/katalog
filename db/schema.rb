ea42c70f2b2b9607d287e35663abedeee1e7f397
Patrick Oscity <patrick.oscity@gmail.com>
2010-05-27 01:02:54 +0200

    added credit model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100526021029) do
+ActiveRecord::Schema.define(:version => 20100526225814) do
+
+  create_table "credits", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.string   "role"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "groups", :force => true do |t|
     t.string   "name"
