027956947ab44cce5c1e4ce88a6cf8fe9ad64a81
Patrick Oscity <patrick.oscity@gmail.com>
2010-05-27 01:32:32 +0200

    associated users with items through credits

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ActiveRecord::Base
+  has_many :credits
+  has_many :users, :through => :credits
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :credits
+  has_many :items, :through => :credits
 end
