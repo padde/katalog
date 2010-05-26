d6c56f31fea24e33869a19de388cabe47b2a8b29
Patrick Oscity <patrick.oscity@gmail.com>
2010-05-26 04:07:40 +0200

    applied item migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100526020258) do
+ActiveRecord::Schema.define(:version => 20100526020602) do
+
+  create_table "items", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
