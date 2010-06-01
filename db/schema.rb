5ce8f3e7c285f3cd2920f4309f108105730e3c63
Patrick Oscity <patrick.oscity@gmail.com>
2010-06-01 22:13:08 +0200

    added account model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100601161808) do
+ActiveRecord::Schema.define(:version => 20100601201237) do
+
+  create_table "accounts", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "person_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "credits", :force => true do |t|
     t.integer  "item_id"
