00c4c162a538dcb0d8472c26a58b7f240a99f3cd
Patrick Oscity <patrick.oscity@gmail.com>
2010-06-02 11:10:39 +0200

    associate users with people through accounts; some cleaning up

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,6 +5,9 @@ class Person < ActiveRecord::Base
   has_many :memberships
   has_many :groups, :through => :memberships
   
+  has_many :accounts
+  has_many :users, :through => :accounts
+  
   def full_name 
     "#{first_name} #{last_name}"
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :accounts
+  has_many :people, :through => :accounts
 end
