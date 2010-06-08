a37b032e836a071bcf79d528a95ebbe832b98323
Patrick Oscity <patrick.oscity@gmail.com>
2010-06-09 01:14:24 +0200

    Enabling Login with username OR email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :username
+  
+  # Enabling Login with username OR email
+  def self.find_for_authentication(conditions={})
+    unless conditions[:email] =~ /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i # email regex
+      conditions[:username] = conditions.delete("email")
+    end
+    super
+  end
+  
+  # validations
 end
