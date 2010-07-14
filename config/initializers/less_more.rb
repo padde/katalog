5d4a182d104f985ac954a91baf0b3bfe4551ef6f
Patrick Oscity <patrick.oscity@gmail.com>
2010-07-14 17:41:52 +0200

    switch prototype & lightbox for jquery & fancybox

---
+++ config/initializers/less_more.rb
@@ -0,0 +1,16 @@
+# Source path: the location of your LESS files
+# (default: app/stylesheets)
+# Less::More.source_path = "public/stylesheets/less"
+
+# Destination Path: where the css goes (public/destination_path)
+# (default: stylesheets)
+# Less::More.destination_path = "css"
+
+# More can compress your files by removing extra line breaks
+# (default: true)
+Less::More.compression = false
+
+# More inserts headers in the generated CSS files, letting people
+# know that the file is in fact generated and shouldn't be edited directly.
+# (default: true)
+Less::More.header = false
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,26 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 function remove_fields(link) {
-  $(link).previous("input[type=hidden]").value = "1";
-  $(link).up(".fields").hide();
-  $(link).up().up(".fields").hide();
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
 }
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).up().insert({
-    before: content.replace(regexp, new_id)
-  });
+  $(link).parent().before(content.replace(regexp, new_id));
 }
+
+$(document).ready(function() {
+  
+  $("a[rel=fancybox]").fancybox({
+    'transitionIn'  : 'none',
+    'transitionOut' : 'none',
+    'titlePosition' : 'over',
+    'titleFormat'   : function(title, currentArray, currentIndex, currentOpts) {
+      return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' +
+      currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+    }
+  });
+  
+});
