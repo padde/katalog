c0bd9515b500068748564587d5e8755ce90e22b8
Patrick Oscity <patrick.oscity@gmail.com>
2010-08-31 19:35:12 +0200

    remove some obsolete comments

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,12 +2,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def new
     build_resource({})
     resource.build_person
-    # debugger
     render_with_scope :new
   end
   
   def edit
-    # @user = User.find(params[:id])
     render_with_scope :edit
   end
 end
