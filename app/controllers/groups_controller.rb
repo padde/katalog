4d04d451bf2693ad73ac504312fe3b60c64de48c
Patrick Oscity <patrick.oscity@gmail.com>
2010-09-01 23:53:32 +0200

    clean up controllers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,43 +2,20 @@ class GroupsController < ApplicationController
   before_filter :authenticate_user!, :except => [:show, :index]
   load_and_authorize_resource
   
-  # GET /groups
-  # GET /groups.xml
   def index
     @groups = Group.asc
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @groups }
-    end
   end
-
-  # GET /groups/1
-  # GET /groups/1.xml
+  
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @group }
-    end
   end
-
-  # GET /groups/new
-  # GET /groups/new.xml
+  
   def new
     @group.memberships.build :person => current_user.person
-    
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @group }
-    end
   end
-
-  # GET /groups/1/edit
+  
   def edit
   end
-
-  # POST /groups
-  # POST /groups.xml
+  
   def create
     @group.user = current_user
     
@@ -48,19 +25,13 @@ class GroupsController < ApplicationController
       params[:group][:website_url] = "http://" + params[:group][:website_url] unless url_regex.match(params[:group][:website_url])
     end
     
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to(@group, :notice => 'Group was successfully created.') }
-        format.xml  { render :xml => @group, :status => :created, :location => @group }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @group.errors, :status => :unprocessable_entity }
-      end
+    if @group.save
+      redirect_to @group, :notice => 'Group was successfully created.'
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /groups/1
-  # PUT /groups/1.xml
   def update
     # TODO: is there a prettier way to do this?
     unless params[:group][:website_url].empty?
@@ -70,25 +41,15 @@ class GroupsController < ApplicationController
     
     @group = Group.find(params[:id], :include => [:memberships,:images])
     
-    respond_to do |format|
-      if @group.update_attributes(params[:group])
-        format.html { redirect_to(@group, :notice => 'Group was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @group.errors, :status => :unprocessable_entity }
-      end
+    if @group.update_attributes(params[:group])
+      redirect_to @group, :notice => 'Group was successfully updated.'
+    else
+      render :action => "edit"
     end
   end
-
-  # DELETE /groups/1
-  # DELETE /groups/1.xml
+  
   def destroy
     @group.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(groups_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to groups_url
   end
 end
--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -2,88 +2,46 @@ class ThingsController < ApplicationController
   before_filter :authenticate_user!, :except => [:show, :index]
   load_and_authorize_resource
   
-  # GET /things
-  # GET /things.xml
   def index
     if params[:own]
       @things = Thing.by_user(current_user).asc
     else
       @things = Thing.asc
     end
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @things }
-    end
   end
-
-  # GET /things/1
-  # GET /things/1.xml
+  
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @thing }
-    end
   end
-
-  # GET /things/new
-  # GET /things/new.xml
+  
   def new
     @thing.credits.build(:person => current_user.person)
-    
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @thing }
-    end
   end
-
-  # GET /things/1/edit
+  
   def edit
   end
-
-  # POST /things
-  # POST /things.xml
+  
   def create
     @thing.user = current_user
     
-    respond_to do |format|
-      if @thing.save
-        format.html { redirect_to(@thing, :notice => 'Thing was successfully created.') }
-        format.xml  { render :xml => @thing, :status => :created, :location => @thing }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @thing.errors, :status => :unprocessable_entity }
-      end
+    if @thing.save
+      redirect_to @thing, :notice => 'Thing was successfully created.'
+    else
+      render :action => "new"
     end
   end
-
-  # PUT /things/1
-  # PUT /things/1.xml
+  
   def update
     @thing = Thing.find(params[:id], :include => [:credits,:images])
     
-    respond_to do |format|
-      if @thing.update_attributes(params[:thing])
-        format.html { redirect_to(@thing, :notice => 'Thing was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @thing.errors, :status => :unprocessable_entity }
-      end
+    if @thing.update_attributes(params[:thing])
+      redirect_to @thing, :notice => 'Thing was successfully updated.'
+    else
+      render :action => "edit"
     end
   end
-
-  # DELETE /things/1
-  # DELETE /things/1.xml
+  
   def destroy
     @thing.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(things_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to things_url
   end
-  
-private
-  
 end
