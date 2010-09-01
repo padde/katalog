bc92cda68234a3d2414009cc479f0c3338aa633f
Patrick Oscity <patrick.oscity@gmail.com>
2010-09-01 19:00:24 +0200

    clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Katalog::Application.routes.draw do |map|
-  resources :groups
-
   devise_for :users,
     :path_names => { :sign_up => 'register', :sign_in => 'log_in' },
     :controllers => { :registrations => "users/registrations" }
   
-  resources :things, :people
+  resources :things, :people, :groups, :contexts
   
-  match :identities, :to => 'people#identities'
+  #match :identities, :to => 'people#identities'
   
   root :to => "things#index"
 end
