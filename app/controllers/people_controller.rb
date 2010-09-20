23bb14b44b9ebdbbdc6db31c2c63bc4caaad3286
Patrick Oscity <patrick.oscity@gmail.com>
2010-09-21 02:29:38 +0800

    re-enable users to create people, but not update them

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,10 +2,42 @@ class PeopleController < ApplicationController
   before_filter :authenticate_user!, :except => [:show, :index]
   load_and_authorize_resource
   
+  respond_to :html
+  
   def index
     @people = Person.asc
+    respond_with @people
   end
   
   def show
+    respond_with @person
+  end
+  
+  def new
+    respond_with @person
+  end
+
+  def edit
+    respond_with @person
+  end
+  
+  def create
+    if @person.save
+      flash[:notice] = 'Person created.'
+    end
+    respond_with @person
+  end
+  
+  def update
+    if @person.update_attributes(params[:kontext])
+      flash[:notice] = 'Person updated.'
+    end
+    respond_with @person
+  end
+  
+  def destroy
+    @person.destroy
+    flash[:notice] = 'Person destroyed.'
+    respond_with @person
   end
 end
