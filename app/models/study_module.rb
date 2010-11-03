f0d2e7786a878aeb41f2858482885917fdd0c03a
Patrick Oscity <patrick.oscity@gmail.com>
2010-11-03 13:18:24 +0100

    add Werkmodul to kinds of study modules

--- app/models/study_module.rb
+++ app/models/study_module.rb
@@ -3,7 +3,7 @@ class StudyModule < ActiveRecord::Base
   has_and_belongs_to_many :things
   has_and_belongs_to_many :teachers, :class_name => 'Person'
   
-  KINDS = %w{ Entwurf Fachkurs Fachmodul Kolloquium Kurs Projekt Projektmodul Seminar Tutorium Übung Vorlesung Workshop Sonstige }
+  KINDS = %w{ Entwurf Fachkurs Fachmodul Kolloquium Kurs Projekt Projektmodul Seminar Tutorium Übung Vorlesung Werkmodul Workshop Sonstige }
   
   scope :asc, order('name')
 end
